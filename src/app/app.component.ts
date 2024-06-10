f0d91d0ace66c6b3f84c342a8cb0e5820358a368
kenken64 <bunnyppl@hotmail.com>
2024-06-10 15:03:37 +0800

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Task } from './model/task.model';
-import { Store } from '@ngrx/store';
-import { TaskState } from './store/todo/task.reducer';
-import { addTask, deleteTask, updateTask, loadTasks} from './store/todo/task.action';
-import { Observable } from 'rxjs';
-import { selectTasks } from './store/todo/task.selector';
+import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,30 +14,4 @@ export class AppComponent implements OnInit{
   ngOnInit(): void {
   }
 
-  // addNewTask(){
-  //   console.log('Adding new task');
-  //   const task: Task = {
-  //     id: Math.floor(Math.random() * 100),
-  //     title: 'New Task',
-  //     description: 'Description',
-  //     priorty: 'Medium',
-  //     status: 0
-  //   };
-  //   console.log('Adding new task');
-    
-  //   this.store.dispatch(addTask({task}));
-  // }
-
-  // updateTask(task: Task){
-  //   const updatedTask: Task = {
-  //     ...task,
-  //     title: 'Updated Task'
-  //   };
-  //   this.store.dispatch(updateTask({task: updatedTask}));
-  // }
-
-  // deleteTask(id: number){
-  //   console.log('Deleting task:', id);
-  //   this.store.dispatch(deleteTask({id}));
-  // }
 }
